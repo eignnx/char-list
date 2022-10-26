e3641444ad230ac0bcb9c392b5a28c90bcb979e9
Eignnx <eignnx@gmail.com>
2022-10-26 15:53:34 -0400

    update docs, add images

--- src/lib.rs
+++ src/lib.rs
@@ -1,106 +1,163 @@
 #![feature(new_uninit, map_first_last, nonzero_min_max, ptr_as_uninit, pattern)]
 #![deny(unsafe_op_in_unsafe_fn)]
 
-//! # CharList
-//! An efficient string type with the same API as a linked-list of characters.
+//!
+// Important: note the blank line of documentation on each side of the image lookup table.
+// The "image lookup table" can be placed anywhere, but we place it here together with the
+// warning if the `doc-images` feature is not enabled.
+#![cfg_attr(feature = "doc-images",
+    cfg_attr(all(),
+    doc = ::embed_doc_image::embed_image!("ex1", "images/char_list_ex_1.svg"),
+    doc = ::embed_doc_image::embed_image!("ex2", "images/char_list_ex_2.svg"),
+    doc = ::embed_doc_image::embed_image!("ex3", "images/char_list_ex_3.svg"),
+    doc = ::embed_doc_image::embed_image!("ex4", "images/char_list_ex_4.svg"),
+))]
+#![cfg_attr(
+    not(feature = "doc-images"),
+    doc = "**Doc images not enabled**. Compile with feature `doc-images` and Rust version >= 1.54 \
+           to enable."
+)]
+//!
+//! A persistent string type with the same API as a linked-list of characters.
 //!
 //! ## DISCLAIMER: `unsafe`
 //! This crate is a work in progress. Specifically ***Not all uses of `unsafe` have been validated!*** Please don't use this for anything serious yet.
 //!
 //! Safety audits are welcome and appreciated! I'm still quite new to writing `unsafe` code.
 //!
+//! Also, this crate depends on [`front-vec`](https://crates.io/crates/front-vec) which is also badly in need of auditing.
+//!
 //! ## Internal Representation
-//! This crate relies on [`front-vec`]("https://crates.io/crates/front-vec") to store arrays of bytes which allow for effiecient prepending (pushing characters onto the front of the array).
+//! This crate relies on [`front_vec::FrontString`](https://docs.rs/front-vec/latest/front_vec/struct.FrontString.html) to store arrays of bytes which allow for efficient prepending (pushing characters onto the front of the array).
 //!
-//! ### `CharList == PqRc<FrontString>`
-//! This crate implements a type called `PqRc` (soon to be moved to its own crate) which stands for "Priority Queue Reference Counted". It's a shared pointer type like `Rc`, but while `Rc<T>` points to a `RcBox<T>` with approximately this layout:
+//! Each `FrontString` is owned by a `PqRcCell` (internal to this crate, soon to be it's own crate) which is sorta like a cross between a `RefCell` and a `RcBox` (the thing an `Rc` pointer points to).
 //!
-//! ```ignore
-//! struct Rc<T> {
-//!     ptr: *mut RcBox<T>,
-//! }
+//! A `CharList` is just a pointer + length (though it's not a slice) which refers to a specific slice of a `PqRcCell`-managed `FrontString`.
 //!
-//! struct RcBox<T> {
-//!     strong: Cell<usize>,
-//!     weak: Cell<usize>,
-//!     value: T,
-//! }
-//! ```
-//!
-//! a `PqRc` points to a `PqRcCell` which looks approximately like this:
-//!
-//! ```ignore
-//! struct PqRc<T, Priority> {
-//!     priority: Priority,
-//!     ptr: *mut PqRcCell<T, Priority>,
-//! }
-//!
-//! struct PqRcCell<T, Priority> {
-//!     priorities: PriorityQueue<(Priority, Count)>,
-//!     value: T,
-//! }
-//! ```
-//!
-//! ### What?
-//!
-//! The cool thing about this setup is that if a `T` is shared among a bunch of `PqRc`s, only those with the **highest priority** are allowed to mutate the inner `T` value. And they can only\* mutate it in ways that other `PqRc`'s of lower priority won't notice.
+//! The `PqRcCell` ensures that out of all `CharList`s which refer to it's `FrontString`, only those with the **longest length** are allowed to mutate the inner `FrontString` value. And they can only\* mutate it in ways that other `CharList`s won't notice.
 //!
 //! \* (Ok, at this point we make developers pinky-swear 🤞 they'll follow those rules, and they gotta wrap their use in an `unsafe` block. Maybe there's a better way to do this in the future though 🤔)
 //!
+//! That was a lotta words, let's look at an example with memory diagrams.
+//!
 //! ### Memory Diagram
 //!
+//! #### Note: Simplified Diagrams
+//! Every time the word `len` or `refs` appears in these diagrams, in the code it corresponds to either `priority` or `priorities`. The internal representation of a `CharList` is actually just a `PqRc` (Priority Queue Reference Counted) pointer which abstracts over its priority. For `CharList`s, priority is the same as string length.
+//!
+//! #### Example
 //! Consider this code:
 //!
-//! ```ignore
-//! let empty = CharList::new();
-//! let yz = empty.cons('z').cons('y');
-//! let wxyz_1 = yz.cons('x').cons('w');
-//! let wxyz_2 = wxyz_1.clone();
+//! ```
+//! # use char_list::CharList;
+//! use assert2::assert; // For nicer assertions.
+//! let icon = CharList::from("icon");
+//! let nomicon = icon.cons_str("nom");
+//! let rustonomicon = nomicon.cons_str("rusto");
+//! let rustonomicon2 = rustonomicon.clone();
+//!
+//! assert!(icon == "icon");
+//! assert!(nomicon == "nomicon");
+//! assert!(rustonomicon == "rustonomicon");
+//! assert!(rustonomicon == rustonomicon2);
 //! ```
 //!
 //! These values would be represented in memory like this:
 //!
-//! ![Box and pointer diagram showing 4 `CharList`s and 1 `PqRcCell` which owns the `FrontString` "wxyz" which also has 4 bytes of free space ahead of it.](assets/char_list_ex_1.svg)
+//! ![did it work?][ex1]
 //!
-//! #### Let's `cons`!
+//! (The arrows with dashed lines represent the *implied* beginnings of `CharList` slices, i.e. `icon` implicitly "refers to" the last four bytes in the buffer.)
 //!
-//! ```ignore
-//! let vwxyz = wxyz_1.cons('v');
+//! Notice that all `cons_str` and `clone` operations are immutable, i.e. they create new `CharList`s instead of mutating the original.
+//!
+//! #### One More `cons`
+//!
 //! ```
-//!
-//! If we `cons` a character onto the front of the string via the variable `wxyz_1`, it's perfectly safe to call the underlying `FrontString`'s `push_char_front` method (mutably!). This is because `wxyz_1` has the highest **priority**. (In the case of `CharList`s, priority is defined as the **length** of the substring.)
-//!
-//! Notice that by pushing a character onto the front, `wxyz_1` doesn't affect **any other `CharList`'s view** of the underlying `FrontString`.
+//! # use char_list::CharList;
+//! # use assert2::assert;
+//! # let icon = CharList::from("icon");
+//! # let nomicon = icon.cons_str("nom");
+//! # let rustonomicon = nomicon.cons_str("rusto");
+//! # let rustonomicon2 = rustonomicon.clone();
+//! #
+//! # assert!(icon == "icon");
+//! # assert!(nomicon == "nomicon");
+//! # assert!(rustonomicon == "rustonomicon");
+//! # assert!(rustonomicon == rustonomicon2);
+//! #
+//! let the_book = rustonomicon.cons_str("the ");
+//! assert!(the_book == "the rustonomicon");
+//! ```
 //!
 //! Here's what memory looks like now:
 //!
-//! ![asdf](assets/char_list_ex_2.svg)
+//! ![did it work?][ex2]
+//!
+//! Because `rustonomicon` has the longest length in the `refs` table, it's perfectly safe to call the underlying `FrontString`'s `push_char_front` method (mutably!).
+//!
+//! Notice that by pushing a character onto the front, `rustonomicon` doesn't affect **any other `CharList`'s view** of the underlying `FrontString`.
 //!
 //! #### Dropping
 //!
-//! Ok now what happens if we `drop` the longest three strings?
+//! Ok now what happens if we `drop` the longest three `CharList`s?
 //!
-//! ```ignore
-//! drop(vwxyz);
-//! drop(wxyz_1);
-//! drop(wxyz_2);
+//! ```
+//! # use char_list::CharList;
+//! # use assert2::assert;
+//! # let icon = CharList::from("icon");
+//! # let nomicon = icon.cons_str("nom");
+//! # let rustonomicon = nomicon.cons_str("rusto");
+//! # let rustonomicon2 = rustonomicon.clone();
+//! #
+//! # assert!(icon == "icon");
+//! # assert!(nomicon == "nomicon");
+//! # assert!(rustonomicon == "rustonomicon");
+//! # assert!(rustonomicon == rustonomicon2);
+//! #
+//! # let the_book = rustonomicon.cons_str("the ");
+//! # assert!(the_book == "the rustonomicon");
+//! #
+//! drop(rustonomicon);
+//! drop(the_book);
+//! drop(rustonomicon2);
 //! ```
 //!
-//! ![asdf](assets/char_list_ex_3.svg)
+//! ![did it work?][ex3]
 //!
-//! Notice that the internal `FrontString`'s `len` went down to 2. This means we can reuse the memory that used to be held by the longer strings!
+//! Notice that the internal `FrontString`'s `len` went down to 7. This means we can reuse the memory that used to be held by the longer strings!
 //!
-//! #### Cons-ing onto `empty`
+//! #### Tricky `cons`
 //!
-//! Here's a problem though. What if we want to `cons` the character `'a'` onto the front of `empty`?
+//! Here's a problem though. What if we want to `cons` the character `'b'` onto the front of `icon`?
 //!
 //! ```ignore
-//! let a = empty.cons('a');
+//! # use char_list::CharList;
+//! # use assert2::assert;
+//! # let icon = CharList::from("icon");
+//! # let nomicon = icon.cons_str("nom");
+//! # let rustonomicon = nomicon.cons_str("rusto");
+//! # let rustonomicon2 = rustonomicon.clone();
+//! #
+//! # assert!(icon == "icon");
+//! # assert!(nomicon == "nomicon");
+//! # assert!(rustonomicon == "rustonomicon");
+//! # assert!(rustonomicon == rustonomicon2);
+//! #
+//! # let the_book = rustonomicon.cons_str("the ");
+//! # assert!(the_book == "the rustonomicon");
+//! #
+//! # drop(rustonomicon);
+//! # drop(the_book);
+//! # drop(rustonomicon2);
+//! #
+//! let janelle_monae = icon.cons('b');
+//! assert!(janelle_monae == "bicon");
 //! ```
 //!
-//! Well, if we overwrite the `'z'` that currently sits there, we'd be modifying `yz`'s view of the string. That's no good, so we gotta **clone** the `FrontString` and mutate the copy.
+//! Well, if we overwrite the `'m'` that currently sits in front of "icon", we'd be modifying `nomicon`'s view of the string. That's no good, so we gotta **clone** `icon`'s view of the `FrontString` and mutate the copy.
 //!
-//! ![adsf](assets/char_list_ex_4.svg)
+//! ![did it work?][ex4]
+//!
 
 mod char_list;
 mod pq_rc;
